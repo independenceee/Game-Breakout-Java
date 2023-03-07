158507f92eb346a148d180647a3a39cb41a72671
Independence <nguyenkhanh17112003@gmail.com>
2023-03-08 01:14:15 +0700

    interface commons attb

---
+++ interfaces/ICommons.java
@@ -0,0 +1,13 @@
+package interface;
+
+public interface ICommons {
+    int WIDTH = 300;
+    int HEIGHT = 400;
+    int BOTTOM_EDGE = 390;
+    int NUMBER_OF_BRICKS = 30;
+    int INITIAL_PADDLE_X = 200;
+    int INITIAL_PADDLE_Y = 360;
+    int INITIAL_BALL_X = 230;
+    int INITIAL_BALL_Y = 355;
+    INT PERIOD = 10;
+}
